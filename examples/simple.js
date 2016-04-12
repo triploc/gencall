b6fa02dfc9b002f48f34e744db618a7e57771b6f
Jonathan Hollinger <jonathanhollinger@gmail.com>
2016-04-12 01:11:52 -0600

    Update static file serving

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,5 @@
 module.exports = function(router, options) {
 
-    console.log(router.paths());
-
     router.call().getpost("/test").params({
         phone: { type: "phone", description: "mobile phone number" },
         email: { type: "email", description: "mail email address" }
--- examples/static/index.js
+++ examples/static/index.js
@@ -1,7 +1,5 @@
 module.exports = function(router, options) {
 
-    console.log(router.paths());
-
     router.static(__dirname + "/content");
 
 };
